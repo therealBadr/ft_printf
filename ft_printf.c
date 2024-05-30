ae043216fd397ba408478069ae8ca9ef693f6a12
therealBadr <b.elabdellaoui101@gmail.com>
2024-05-30 20:48:50 +0100

    ft_putnbr_u && ft_putnbr_hex

--- ft_printf.c
+++ ft_printf.c
@@ -39,6 +39,32 @@ void	ft_putnbr(int n, int *count)
 	}
 }
 
+void	ft_putnbr_u(unsigned int n, int *count)
+{
+	if (n < 10)
+		ft_putchar(n + '0', count);
+	else
+	{
+		ft_putnbr(n / 10, count);
+		ft_putnbr(n % 10, count);
+	}
+}
+
+void	ft_putnbr_hex(unsigned int n, char s, int *count)
+{
+	char		*hex;
+	
+	if (s == 'x')
+		hex = "0123456789abcdef";
+	else
+		hex = "0123456789ABCDEF";
+	if (n < 16)
+		ft_putchar(hex[n], count);
+	else
+		ft_putnbr(n / 16, s, count);
+		ft_putnbr(n % 16, s, count);
+}
+
 void	check_type(va_list argp, const char str, int *count)
 {
 	// Conversions to implement:
@@ -49,7 +75,24 @@ void	check_type(va_list argp, const char str, int *count)
 	// %s - printing a string
 	else if (str == 's')
 		ft_putstr(var_arg(argp, int), count);
-	//else if ()
+	// %d && %i = base 10
+	else if (str == 'd' || str == 'i')
+		ft_putnbr(var_arg(argp, int), count);
+	// %u Prints an unsigned decimal (base 10) number
+	else if (str == 'u')
+		ft_putnbr_u(var_arg(argp, unsigned int), count);
+	// %% Prints a percent sign.
+	else if (str == '%')
+		ft_putchar('%', count);
+	// %x Prints a number in hexadecimal (base 16) lowercase format.
+	else if (str == 'x')
+		ft_putnbr_base(var_arg(argp, unsigned int), str, count);
+	// %X Prints a number in hexadecimal (base 16) uppercase format
+	else if (str == 'X')
+		ft_putnbr_base(var_arg(argp, unsigned int), str, count);
+	// The void * pointer argument has to be printed in hexadecimal
+	else if (str == 'p')
+		
 }
 
 int	ft_printf(const char *str, ...)
